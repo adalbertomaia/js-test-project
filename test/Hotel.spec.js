55343ff0513d64e5d04f193d5cf0609f02e07032
Adalberto <adalberto.maia@gmail.com>
2019-10-24 11:49:31 -0300

    Refactor Hotel test

--- test/Hotel.spec.js
+++ test/Hotel.spec.js
@@ -4,13 +4,6 @@ const chai = require('chai')
 const expect = chai.expect
 
 describe('Hotel', function () {
-    it('should be created', function () {
-        const Hotel = require('../src/Hotel.js')
-        const hotelInstance = new Hotel()
-
-        expect(hotelInstance).to.not.be.undefined
-    })
-
     it('should find best hotel', function(){
         const Hotel = require('../src/Hotel')
         const hotelInstance = new Hotel()
