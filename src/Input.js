a457e74f96ef39ab44810b7d5ac82fd7133df05d
Adalberto <adalberto.maia@gmail.com>
2019-10-24 13:37:35 -0300

    Add method getClientType

--- src/Input.js
+++ src/Input.js
@@ -1,5 +1,9 @@
 class Input {
 
+    getClientType() {
+        return "Regular"
+    }
+
 }
 
 module.exports = Input
--- test/Input.spec.js
+++ test/Input.spec.js
@@ -3,11 +3,20 @@
 const chai = require('chai')
 const expect = chai.expect
 
-describe('Input', function(){
-    it('should be created', function(){
+describe('Input', function () {
+    it('should be created', function () {
         const Input = require('../src/Input')
         const inputInstance = new Input()
 
         expect(inputInstance).to.not.be.undefined
+    })
+
+    it('get client type', function () {
+        const Input = require('../src/Input')
+        const inputInstance = new Input()
+        const actual = 'Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)'
+        const expected = 'Regular'
+
+        expect(inputInstance.getClientType()).to.eq(expected)
     })
 })
