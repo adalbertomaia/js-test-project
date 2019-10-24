b3297eae50fd7bbac3b2d6748181e003801254e9
Adalberto <adalberto.maia@gmail.com>
2019-10-24 14:10:02 -0300

    Create method getHotelDatabase

--- src/Hotel.js
+++ src/Hotel.js
@@ -4,6 +4,9 @@ class Hotel {
         return "Lakewood"
     } 
 
+    getHotelDatabase() {
+        return require('../database/hotels.json')
+    }
 }
 
 module.exports = Hotel
--- test/Hotel.spec.js
+++ test/Hotel.spec.js
@@ -4,11 +4,19 @@ const chai = require('chai')
 const expect = chai.expect
 
 describe('Hotel', function () {
+    const Hotel = require('../src/Hotel')
+    const hotelInstance = new Hotel()
+
     it('should find best hotel', function(){
-        const Hotel = require('../src/Hotel')
-        const hotelInstance = new Hotel()
         const expected = 'Lakewood'
         
         expect(hotelInstance.findBestHotel()).to.eql(expected)
+    })
+
+    it('get hotel database', function(){       
+        const expected = require('../database/hotels.json')
+
+        expect(hotelInstance.getHotelDatabase()).to.eql(expected)
+        
     })
 })
