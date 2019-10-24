99b3af83be0ec23e2331ead1c3146a0193a719eb
Adalberto <adalberto.maia@gmail.com>
2019-10-24 14:19:39 -0300

    Implement getHotelIndex

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,11 +1,22 @@
 class Hotel {
 
     findBestHotel() {
-        return "Lakewood"
-    } 
+        return 'Lakewood'
+    }
 
     getHotelDatabase() {
         return require('../database/hotels.json')
+    }
+
+    getHotelIndex(hotel) {
+        if (hotel === 'Lakewood') {
+            return 0
+        } else if (hotel ==='Bridgewood') {
+            return 1
+        } else {
+            return 2
+        }
+
     }
 }
 
--- test/Hotel.spec.js
+++ test/Hotel.spec.js
@@ -10,13 +10,25 @@ describe('Hotel', function () {
     it('should find best hotel', function(){
         const expected = 'Lakewood'
         
-        expect(hotelInstance.findBestHotel()).to.eql(expected)
+        expect(hotelInstance.findBestHotel()).to.eq(expected)
     })
 
     it('get hotel database', function(){       
         const expected = require('../database/hotels.json')
 
         expect(hotelInstance.getHotelDatabase()).to.eql(expected)
-        
+    })
+
+    it('get hotel index', function(){
+        const lakewood = 'Lakewood'
+        const bridgewood = 'Bridgewood'
+        const ridgewood = 'Ridgewood'
+        const expectedLakewood = 0
+        const expectedBridgewood = 1
+        const expectedRigdewood = 2
+
+        expect(hotelInstance.getHotelIndex(lakewood)).to.eq(expectedLakewood)
+        expect(hotelInstance.getHotelIndex(bridgewood)).to.eq(expectedBridgewood)
+        expect(hotelInstance.getHotelIndex(ridgewood)).to.eq(expectedRigdewood)
     })
 })
